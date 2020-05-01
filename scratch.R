3d770d04ce6d50b5a58f3dc823a8b97e0e003439
Sydney Sorkin <ssorkin@college.harvard.edu>
2020-05-01 17:59:49 -0500

    testing methods

--- scratch.R
+++ scratch.R
@@ -1,12 +1,59 @@
 library(rtweet)
 library(ggmap)
+library(tidytext)
 library(tidyverse)
 
+google_api_key <- "AIzaSyAVk2Tfe0cYOyMuXdQNSixn3Sum-TxXlzM"
+register_google(key = google_api_key)
 
 #sandy hook example 
-
-sandy_hook <- search_fullarchive('"Sandy Hook"',
-                            n = 10, 
+sandy_hook1 <- search_fullarchive('"Sandy Hook"',
+                            n = 100, 
                             fromDate = "2012-12-14",
                             toDate = "2012-12-15",
                             env_name = "dev")
+
+sandyhook2 <- search_fullarchive('"Sandy Hook"',
+                                 n = 100, 
+                                 fromDate = "2012-12-15",
+                                 toDate = "2012-12-16",
+                                 env_name = "dev")
+
+sandyhook3 <- search_fullarchive('"Sandy Hook"',
+                                 n = 100, 
+                                 fromDate = "2012-12-16",
+                                 toDate = "2012-12-17",
+                                 env_name = "dev")
+
+combined <- rbind(sandy_hook1, sandyhook2, sandyhook3)
+
+##gun language by state?
+
+IL <- search_tweets("gun OR firearm filter:verified", 
+                   n=100,
+                   include_rts = FALSE,
+                   geocode = lookup_coords("usa", apikey = google_api_key))
+
+
+
+
+####analysis of cnn timeline 
+stuff <- get_timeline(user = "@cnn", n = 3200)
+
+
+  
+stuff$stripped_text <- gsub("http.*","",  stuff$text)
+stuff$stripped_text <- gsub("https.*","", stuff$stripped_text)
+
+
+stuff_clean <- stuff %>% 
+  unnest_tokens(word, stripped_text) %>% 
+  anti_join(stop_words) %>% 
+  select(status_id, text, location, word) %>% 
+  mutate(trump = ifelse(word == "trump", 1, 0)) %>% 
+  group_by(status_id) %>% 
+  mutate(trump_logical  = sum(trump)) %>% 
+  filter(trump_logical == 1) 
+  
+
+  
